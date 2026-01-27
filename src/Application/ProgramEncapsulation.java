5bcf545fbe8fbc727e517f245604b8e42add8b46
Gustavo <gustavotelo007@gmail.com>
2026-01-27 16:50:01 -0300

    Adding the class exercise, the class and the program, and adding the encapsulation exercise class.

---
+++ src/Application/ProgramEncapsulation.java
@@ -0,0 +1,10 @@
+package Application;
+
+public class ProgramEncapsulation {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/Entities/Account.java
@@ -0,0 +1,49 @@
+package Entities;
+
+public class Account {
+
+	 private Integer number;
+	 private String holder;
+	 private Double balance;
+	 
+	 public Account(Integer number, String holder) {
+			this.number = number;
+			this.holder = holder;
+		 }	
+
+	 public Account(Integer number, String holder, Double initialDeposit) {
+		this.number = number;
+		this.holder = holder;
+		deposit(initialDeposit);
+	 }
+	 
+	 public Integer getNumber() {
+		 return number;
+	 }
+	 
+	 public String getHolder() {
+		 return holder;
+	 }
+	 public void setHolder(String holder) {
+		 this.holder = holder;
+	 }
+	 public Double getBalance() {
+		 return balance;
+	 }	
+	 
+	 public void deposit(double amount) {
+		 this.balance += amount;
+	 }	
+	 
+	 public void withdraw(double amount) {
+		 this.balance -= amount - 5.00;
+	 }
+
+	 @Override
+	 public String toString() {
+		return "Account [number=" + number + ", holder=" + holder + ", balance=" + String.format(".2f%", balance) + "]";
+	 }
+	 
+	 
+	 
+}
